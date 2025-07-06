7b05dd770e6a51a7512a7ff1110fe46f4475687d
M4RC0Sx <marcos3as3@gmail.com>
2025-07-07 00:03:07 +0200

    chore: eslint + prettier + isort

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ["dist"],
+  },
+
+  {
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      "react-hooks": pluginHooks,
+      "react-refresh": pluginReactRefresh,
+    },
+    rules: {
+      // Existing plugins
+      ...pluginReact.configs.recommended.rules,
+      ...pluginHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": "warn",
     },
   },
-])
+
+  eslintConfigPrettier
+);
