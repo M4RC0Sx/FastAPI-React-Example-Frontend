6be3a437db48887ef4f3f77aa9ce5ee953275933
M4RC0Sx <marcos3as3@gmail.com>
2025-07-07 00:09:28 +0200

    chore: set up tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="bg-amber-700">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
