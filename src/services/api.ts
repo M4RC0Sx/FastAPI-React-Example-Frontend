dc6e128566580021d5ad032fd7a97c14a81e1ca2
M4RC0Sx <marcos3as3@gmail.com>
2025-07-07 20:20:43 +0200

    feat: create api service

---
+++ src/services/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+
+const api = axios.create({
+  baseURL: baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
